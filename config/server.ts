36e948cc0319bfa6773a5f3167708d47c3be63e4
Jatt <jatt.jatt@gmail.com>
2025-11-05 00:42:50 -0500

    update strapi to v5.30 and update plugins

--- config/server.ts
+++ config/server.ts
@@ -3,6 +3,7 @@ import cronTask from './cron-tasks';
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  proxy: {koa: true},    
   cron: {
     enabled: true,
     tasks: cronTask,
